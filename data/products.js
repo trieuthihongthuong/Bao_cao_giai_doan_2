d8cb8f2019f65a8f7e77bc99d735cad3ff4dfafb
trieuthihongthuong <trieuthihongthuong49@gmail.com>
2024-05-08 21:37:13 +0700

    bao_cao_tien_do

---
+++ data/products.js
@@ -0,0 +1,639 @@
+var list_products = [{
+    
+    //samsung product
+    
+    "name": "Samsung Galaxy S24 Ultra",
+    "company": "Samsung",
+    "img": "img/products/samsung/1.samsung-galaxy-s24-ultra-5g-sm-s928-stylus.jpg",
+    "price": "43.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "giareonline",
+        "value": "34.000.000"
+    },
+    "detail": {
+        "screen": "Dynamic LTPO AMOLED 2X, 120Hz, HDR10+, 2600 nits (peak)",
+        "os": "Android 14, One UI 6.1",
+        "camara": "48 MP",
+        "camaraFront": "12 MP",
+        "cpu": "8-core (1x3.39GHz Cortex-X4 & 3x3.1GHz Cortex-A720 & 2x2.9GHz Cortex-A720 & 2x2.2GHz Cortex-A520)",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "5200 mAh"
+    },
+    "masp": "Sam0"
+}, 
+
+{
+    "name": "Samsung Galaxy S24+",
+    "company": "Samsung",
+    "img": "img/products/samsung/2.samsung-galaxy-s24-plus-5g-sm-s926.jpg",
+    "price": "36.690.000",
+    "star": 5,
+    "rateCount": 9,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Sam1"
+}, 
+
+{
+    "name": "Samsung Galaxy c55",
+    "company": "Samsung",
+    "img": "img/products/samsung/3.samsung-galaxy-c55.jpg",
+    "price": "36.690.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "giareonline",
+        "value": "32.000.000"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Sam2"
+}, 
+
+{
+    "name": "Samsung Galaxy c55",
+    "company": "Samsung",
+    "img": "img/products/samsung/4.samsung-galaxy-a55.jpg",
+    "price": "36.690.000",
+    "star": 5,
+    "rateCount": 8,
+    "promo": {
+        "name": "giareonline",
+        "value": "31.000.000"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Sam3"
+}, 
+
+
+
+
+//oppo product
+
+
+{
+    "name": "Oppo A60",
+    "company": "Oppo",
+    "img": "img/products/oppo/1.oppo-a60.jpg",
+    "price": "22.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "LTPS LCD, 6.3', Full HD+",
+        "os": "ColorOS 5.2 (Android 8.1)",
+        "camara": "16 MP và 2 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "MediaTek Helio P60 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Opp0"
+}, 
+
+{
+    "name": "Oppo K12",
+    "company": "Oppo",
+    "img": "img/products/oppo/2.oppo-k12.jpg",
+    "price": "18.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "LTPS LCD, 6.3', Full HD+",
+        "os": "ColorOS 5.2 (Android 8.1)",
+        "camara": "16 MP và 2 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "MediaTek Helio P60 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Opp1"
+}, 
+
+
+{
+    "name": "Oppo A13",
+    "company": "Oppo",
+    "img": "img/products/oppo/3.oppo-a3-pro.jpg",
+    "price": "32.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "tragop",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "LTPS LCD, 6.3', Full HD+",
+        "os": "ColorOS 5.2 (Android 8.1)",
+        "camara": "16 MP và 2 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "MediaTek Helio P60 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Opp2"
+}, 
+
+{
+    "name": "Oppo F25",
+    "company": "Oppo",
+    "img": "img/products/oppo/4.oppo-f25-pro.jpg",
+    "price": "7.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "tragop",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "LTPS LCD, 6.3', Full HD+",
+        "os": "ColorOS 5.2 (Android 8.1)",
+        "camara": "16 MP và 2 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "MediaTek Helio P60 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Opp3"
+}, 
+
+
+
+
+
+
+
+{
+    "name": "iPhone 15 Pro Max",
+    "company": "Apple",
+    "img": "img/products/iphone/1.iPhone_15_Pro_Max.jpg",
+    "price": "31.990.000",
+    "star": 5,
+    "rateCount": 12,
+    "promo": {
+        "name": "moiramat",
+        "value": "47.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.8', Super Retina",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "2716 mAh, có sạc nhanh"
+    },
+    "masp": "App0"
+},  
+
+
+{
+    "name": "iPhone 15 Pro",
+    "company": "Apple",
+    "img": "img/products/iphone/2.apple-iphone-15-pro.jpg",
+    "price": "31.990.000",
+    "star": 5,
+    "rateCount": 30,
+    "promo": {
+        "name": "moiramat",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.8', Super Retina",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "2716 mAh, có sạc nhanh"
+    },
+    "masp": "App1"
+},  
+
+{
+    "name": "iPhone 15 plus",
+    "company": "Apple",
+    "img": "img/products/iphone/3.apple-iphone-15-plus-.jpg",
+    "price": "35.990.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "moiramat",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.8', Super Retina",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "2716 mAh, có sạc nhanh"
+    },
+    "masp": "App2"
+},  
+
+
+
+{
+    "name": "iPhone 15 pro max",
+    "company": "Apple",
+    "img": "img/products/iphone/4.apple-iphone-14-pro-max-.jpg",
+    "price": "36.990.000",
+    "star": 4,
+    "rateCount": 15,
+    "promo": {
+        "name": "moiramat",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.8', Super Retina",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "2716 mAh, có sạc nhanh"
+    },
+    "masp": "App3"
+},  
+
+
+
+
+//Huawei products
+
+{
+    "name": "Huawei Pura 70 Ultra",
+    "company": "Huawei",
+    "img": "img/products/huawei/1.huawei-pura70-ultra.jpg",
+    "price": "24.990.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "OLED, 6.39', Quad HD+ (2K+)",
+        "os": "Android 9.0 (Pie)",
+        "camara": "40 MP, 20 MP và 8 MP (3 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Hisilicon Kirin 980 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "NM card, hỗ trợ tối đa 512 GB",
+        "battery": "4200 mAh, có sạc nhanh"
+    },
+    "masp": "Hua0"
+}, 
+
+{
+    "name": "Huawei Mate 20 Pro",
+    "company": "Huawei",
+    "img": "img/products/huawei/2.huawei-pura70-pro-plus.jpg",
+    "price": "41.990.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "giamgia",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "OLED, 6.39', Quad HD+ (2K+)",
+        "os": "Android 9.0 (Pie)",
+        "camara": "40 MP, 20 MP và 8 MP (3 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Hisilicon Kirin 980 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "NM card, hỗ trợ tối đa 512 GB",
+        "battery": "4200 mAh, có sạc nhanh"
+    },
+    "masp": "Hua1"
+}, 
+
+
+{
+    "name": "Huawei nova 11",
+    "company": "Huawei",
+    "img": "img/products/huawei/3.huawei-nova-11-se.jpg",
+    "price": "29.990.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "giamgia",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "OLED, 6.39', Quad HD+ (2K+)",
+        "os": "Android 9.0 (Pie)",
+        "camara": "40 MP, 20 MP và 8 MP (3 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Hisilicon Kirin 980 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "NM card, hỗ trợ tối đa 512 GB",
+        "battery": "4200 mAh, có sạc nhanh"
+    },
+    "masp": "Hua2"
+}, 
+
+{
+    "name": "Huawei nova 11 se",
+    "company": "Huawei",
+    "img": "img/products/huawei/4.huawei-nova-11-se.jpg",
+    "price": "29.990.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "giareonline",
+        "value": "27.000.000"
+    },
+    "detail": {
+        "screen": "OLED, 6.39', Quad HD+ (2K+)",
+        "os": "Android 9.0 (Pie)",
+        "camara": "40 MP, 20 MP và 8 MP (3 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Hisilicon Kirin 980 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "NM card, hỗ trợ tối đa 512 GB",
+        "battery": "4200 mAh, có sạc nhanh"
+    },
+    "masp": "Hua3"
+}, 
+
+
+
+
+
+
+//Realme product
+
+{
+    "name": "Realme 12x (China)",
+    "company": "Realme",
+    "img": "img/products/realme/1.realme-12x.jpg",
+    "price": "9.490.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.2', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP và 2 MP (2 camera)",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "4230 mAh"
+    },
+    "masp": "Rea0"
+}, 
+
+
+{
+    "name": "Realme 12x (China)",
+    "company": "Realme",
+    "img": "img/products/realme/2.realme-narzo-70.jpg",
+    "price": "8.490.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.2', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP và 2 MP (2 camera)",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "4230 mAh"
+    },
+    "masp": "Rea1"
+}, 
+
+{
+    "name": "Realme norzo 70x",
+    "company": "Realme",
+    "img": "img/products/realme/3.realme-narzo-70x.jpg",
+    "price": "21.490.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.2', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP và 2 MP (2 camera)",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "4230 mAh"
+    },
+    "masp": "Rea2"
+}, 
+
+{
+    "name": "Realme c67 lite",
+    "company": "Realme",
+    "img": "img/products/realme/4.realme-c67-lte.jpg",
+    "price": "6.490.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.2', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP và 2 MP (2 camera)",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "4230 mAh"
+    },
+    "masp": "Rea3"
+}, 
+
+
+
+
+
+{
+    "name": "vivo iQOO Z9 Turbo",
+    "company": "Vivo",
+    "img": "img/products/vivo/1.vivo-iqoo-z9-turbo.jpg",
+    "price": "10.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "9.990.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.41', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3400 mAh, có sạc nhanh"
+    },
+    "masp": "Viv0"
+}, 
+
+{
+    "name": "vivo iQOO Z9x",
+    "company": "Vivo",
+    "img": "img/products/vivo/2.vivo-iqoo-z9x.jpg",
+    "price": "9.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.41', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3400 mAh, có sạc nhanh"
+    },
+    "masp": "Viv1"
+}, 
+
+{
+    "name": "vivo t3x",
+    "company": "Vivo",
+    "img": "img/products/vivo/3.vivo-t3x.jpg",
+    "price": "6.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.41', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3400 mAh, có sạc nhanh"
+    },
+    "masp": "Viv2"
+}, 
+
+
+{
+    "name": "vivo v30 lite",
+    "company": "Vivo",
+    "img": "img/products/vivo/4.vivo-v30-lite-4g.jpg",
+    "price": "5.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.41', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "25 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3400 mAh, có sạc nhanh"
+    },
+    "masp": "Viv2"
+}, 
+
+]
